0fe4ef6a082104e1e95c96d15090ee7cb2ff278e
Luna <lunawu@bu.edu>
2019-11-15 11:08:57 -0500

    update the association rule

--- Initial.R
+++ Initial.R
@@ -1,11 +1,13 @@
+
+# install the packages
 library(readr)
 library(tidyverse)
 
+# load the datasets
+customer <- read.csv('data/olist_customers_dataset.csv')
+order_item <- read.csv('data/olist_order_items_dataset.csv')
+order_payment <- read.csv('data/olist_order_payments_dataset.csv')
+order_reviews <- read.csv('data/olist_order_reviews_dataset.csv')
+order <- read.csv('data/olist_orders_dataset.csv')
+order_product <- read.csv('data/olist_products_dataset.csv')
 
-customer <- read.csv('olist_customers_dataset.csv')
-order_item <- read.csv('olist_order_items_dataset.csv')
-order_item <- read.csv('olist_order_items_dataset.csv')
-order_payment <- read.csv('olist_order_payments_dataset.csv')
-order_reviews <- read.csv('olist_order_reviews_dataset.csv')
-order <- read.csv('olist_orders_dataset.csv')
-order_product <- read.csv('olist_products_dataset.csv')
---
+++ association_rule.R
@@ -0,0 +1,5 @@
+# This file intends to use association rules
+# to explore the recognize the transactional pattern 
+# to future determine the co-occurence between items.
+# The application of the association rules is to 
+# 
