3c69c3189b34839c02bf308041668cc78a52b3a3
siyu-liu315 <53445006+siyu-liu315@users.noreply.github.com>
2019-11-16 00:01:46 +0800

    upate join table

--- Initial.R
+++ Initial.R
@@ -1,11 +1,15 @@
 library(readr)
 library(tidyverse)
 
-#####
-customer <- read.csv('olist_customers_dataset.csv')
-order_item <- read.csv('olist_order_items_dataset.csv')
-order_item <- read.csv('olist_order_items_dataset.csv')
-order_payment <- read.csv('olist_order_payments_dataset.csv')
-order_reviews <- read.csv('olist_order_reviews_dataset.csv')
-order <- read.csv('olist_orders_dataset.csv')
-order_product <- read.csv('olist_products_dataset.csv')
+#####reading files
+customer <- read.csv('data/olist_customers_dataset.csv')
+order_item <- read.csv('data/olist_order_items_dataset.csv')
+order_item <- read.csv('data/olist_order_items_dataset.csv')
+order_payment <- read.csv('data/olist_order_payments_dataset.csv')
+order_reviews <- read.csv('data/olist_order_reviews_dataset.csv')
+order <- read.csv('data/olist_orders_dataset.csv')
+order_product <- read.csv('data/olist_products_dataset.csv')
+product <- read.csv('data/olist_products_dataset.csv')
+
+#######
+transaction<- left_join(left_join(left_join(customer, order, by = 'customer_id'),order_item,by = 'order_id'),order_product, by = 'product_id')
