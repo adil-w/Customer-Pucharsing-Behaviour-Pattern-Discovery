9248b56d7773c9f01192f28773e6329bea42a087
siyu-liu315 <53445006+siyu-liu315@users.noreply.github.com>
2019-11-15 23:38:31 +0800

    update file readings

---
+++ Initial.R
@@ -0,0 +1,11 @@
+library(readr)
+library(tidyverse)
+
+
+customer <- read.csv('olist_customers_dataset.csv')
+order_item <- read.csv('olist_order_items_dataset.csv')
+order_item <- read.csv('olist_order_items_dataset.csv')
+order_payment <- read.csv('olist_order_payments_dataset.csv')
+order_reviews <- read.csv('olist_order_reviews_dataset.csv')
+order <- read.csv('olist_orders_dataset.csv')
+order_product <- read.csv('olist_products_dataset.csv')
