b79787f18f613e486235748191d652535200f7bb
siyu-liu315 <53445006+siyu-liu315@users.noreply.github.com>
2019-11-16 00:02:10 +0800

    update join table

--- Initial.R
+++ Initial.R
@@ -10,6 +10,9 @@ order_reviews <- read.csv('data/olist_order_reviews_dataset.csv')
 order <- read.csv('data/olist_orders_dataset.csv')
 order_product <- read.csv('data/olist_products_dataset.csv')
 product <- read.csv('data/olist_products_dataset.csv')
+location <- read.csv('data/olist_geolocation_dataset.csv')
+
 
 #######
-transaction<- left_join(left_join(left_join(customer, order, by = 'customer_id'),order_item,by = 'order_id'),order_product, by = 'product_id')
+p<- left_join(left_join(left_join(customer, order, by = 'customer_id'),order_item,by = 'order_id'),order_product, by = 'product_id')
+transaction <- left_join(p,order_payment, by = "order_id")
