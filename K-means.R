6f8bc63b87564eec729961daa260985dcd25f621
Adil <adilw@bu.edu>
2019-11-19 19:07:13 +0000

    added different methods for determining optimal clusters

--- K-means.R
+++ K-means.R
@@ -7,7 +7,7 @@ library(factoextra)
 library(tidyverse)
 library(skimr)
 
-skim(transaction)
+set.seed(123)
 
 t_k = transaction %>% select_if(Negate(is.factor))
 t_k1 = t_k %>% select(-customer_zip_code_prefix)
@@ -15,10 +15,50 @@ t_k1[is.na(t_k1)] <- 0
 sapply(t_k1, as.numeric)
 t_k1 = t_k1 %>% select(-order_id)
 
+### scale the data
 j = scale(t_k1)
 
-k = kmeans(j, centers=3, iter.max=25, nstart=25)
+### WSS analysis
+k_wss = function(k) {
+  km = kmeans(j, k, nstart=25, iter=25)
+  kwss = km$tot.withinss
+  return(kwss)
+}
+
+x = 1:15
+wss_vals = map_dbl(x, k_wss)
+
+plot(x, wss_values,
+     type="b", pch = 19, frame = FALSE,
+     main= "Transactions Select K - WSS",
+     xlab= "Number of clusters K",
+     ylab= "Total within-clusters sum of squares")
+
+plot(x, wss_vals, type="b", main = "Transactions Select K - WSS")
+
+### cluster plot 
+k = kmeans(j, centers=10, iter.max=25, nstart=25)
 fviz_cluster(k, data=j)
+
+# function to compute average silhouette for k clusters
+avg_sil <- function(k) {
+  km.res <- kmeans(j, centers = k, nstart = 25)
+  ss <- silhouette(km.res$cluster, dist(j))
+  mean(ss[, 3])
+}
+
+# Compute and plot wss for k = 2 to k = 15
+k.values <- 2:15
+
+# extract avg silhouette for 2-15 clusters
+avg_sil_values <- map_dbl(k.values, avg_sil)
+
+plot(k.values, avg_sil_values,
+     type = "b", pch = 19, frame = FALSE, 
+     xlab = "Number of clusters K",
+     ylab = "Average Silhouettes")
+
+plot(silhouette(k$cluster, dist=dist(j)), col=1:3)
 
 names(k)
 k$cluster
@@ -29,18 +69,13 @@ k$size
 table(k$cluster)
 k$centers
 
-k_wss = function(k) {
-  km = kmeans(j, k, nstart=25, iter=25)
-  kwss = km$tot.withinss
-  return(kwss)
-}
-
-x = 1:15
-wss_vals = map_dbl(x, k_wss)
-
-plot(x, wss_vals, type="b", main = "Transactions Select K - WSS")
 
 fviz_nbclust(j,
              kmeans,
              method="silhouette",
              k.max=20)
+
+########################################################################
+t_dist = dist(j)
+h1 = hclust(t_dist)
+plot(h1)
