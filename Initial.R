7bc2b190ca0ceaea688fe50e037aa734c2fcf63a
siyu-liu315 <53445006+siyu-liu315@users.noreply.github.com>
2019-11-20 11:05:13 +0800

    update combine

--- Initial.R
+++ Initial.R
@@ -8,6 +8,10 @@ library(purrr)
 library(cluster)
 library(factoextra)
 library(corrplot)
+library(arules)
+library(arulesViz)
+library(psych)
+library(GPArotation)
 
 
 # load the datasets
@@ -67,15 +71,85 @@ fviz_screeplot(transaction2_pca, addlabels=T, ylim =c(0,100))
 get_eigenvalue(transaction2_pca)
 fviz_pca_contrib(transaction2_pca, choice="var")
 
-## EFA
 
+
+## EFA
 # Data Assessment 
 transaction2_cor
 cortest.bartlett(transaction2_cor, nrow(transaction2))
-KMO(transaction_cor)
+KMO(transaction2_cor)
 
 
-## Clean data
+## Clustering
+
+##  choose numerical value
+t_k = transaction2 %>% select_if(Negate(is.factor))
+
+### scale the data
+j = scale(t_k)
+
+### WSS analysis
+k_wss = function(k) {
+  km = kmeans(j, k, nstart=25, iter=25)
+  kwss = km$tot.withinss
+  return(kwss)
+}
+
+x = 1:15
+wss_vals = map_dbl(x, k_wss)
+
+plot(x, wss_vals,
+     type="b", pch = 19, frame = FALSE,
+     main= "Transactions Select K - WSS",
+     xlab= "Number of clusters K",
+     ylab= "Total within-clusters sum of squares")
+
+plot(x, wss_vals, type="b", main = "Transactions Select K - WSS")
+
+### cluster plot 
+k = kmeans(j, centers=10, iter.max=25, nstart=25)
+fviz_cluster(k, data=j)
+
+# function to compute average silhouette for k clusters
+avg_sil <- function(k) {
+  km.res <- kmeans(j, centers = k, nstart = 25)
+  ss <- silhouette(km.res$cluster, dist(j))
+  mean(ss[, 3])
+}
+
+# Compute and plot wss for k = 2 to k = 15
+k.values <- 2:15
+
+# extract avg silhouette for 2-15 clusters
+avg_sil_values <- map_dbl(k.values, avg_sil)
+
+plot(k.values, avg_sil_values,
+     type = "b", pch = 19, frame = FALSE, 
+     xlab = "Number of clusters K",
+     ylab = "Average Silhouettes")
+
+plot(silhouette(k$cluster, dist=dist(j)), col=1:3)
+
+names(k)
+k$cluster
+k$centers
+table(k$cluster)
+k$size
+
+table(k$cluster)
+k$centers
+
+
+fviz_nbclust(j,
+             kmeans,
+             method="silhouette",
+             k.max=20)
+
+########################################################################
+t_dist = dist(j)
+h1 = hclust(t_dist)
+plot(h1)
+
 
 
 
